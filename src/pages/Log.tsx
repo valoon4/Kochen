158e38bee70756c2f3acc9b80946580f1368bf8f
valoon4 <jenjasy@web.de>
2024-11-14 16:00:20 +0100

    Added details

--- src/pages/Log.tsx
+++ src/pages/Log.tsx
@@ -7,13 +7,42 @@ const MainView = () => {
     <main>
       <h1>Project Logs</h1>
       <NavigationManager />
+
       <h3>Anime: </h3>
+      <details>
+      <summary>Comic Updates</summary>
+      <p>EMPTY</p>
+      </details>
+
       <h3>Manga: </h3>
+      <details>
+      <summary>Comic Updates</summary>
+      <p>EMPTY</p>
+      </details>
+
       <h3>Cartoon: </h3>
+      <details>
+      <summary>Comic Updates</summary>
+      <p>EMPTY</p>
+      </details>
+
       <h3>Comic: Version 13.11 </h3>
-      <ul><li>BRZRKR 04</li></ul>
+      <details>
+      <summary>Comic Updates</summary>
+      <p>BRZRKR 04</p>
+      </details>
+
       <h3>Live Action: </h3>
+      <details>
+      <summary>Comic Updates</summary>
+      <p>EMPTY</p>
+      </details>
+
       <h3>Game: </h3>
+      <details>
+      <summary>Comic Updates</summary>
+      <p>EMPTY</p>
+      </details>
 
     </main >
   );
