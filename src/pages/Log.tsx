5fda96b5f623e805195318ab9ce9d6eb33093cc2
valoon4 <jenjasy@web.de>
2024-11-14 15:49:13 +0100

    Create Log.tsx

---
+++ src/pages/Log.tsx
@@ -0,0 +1,22 @@
+import { NavigationManager } from '@/utils/NavigationManager';
+import React from 'react';
+
+const MainView = () => {
+
+  return (
+    <main>
+      <h1>Project Logs</h1>
+      <NavigationManager />
+      <p>Anime: </p>
+      <p>Manga: </p>
+      <p>Cartoon: </p>
+      <p>Comic: Version 13.11 </p>
+      BRZRKR 04
+      <p>Live Action: </p>
+      <p>Game: </p>
+
+    </main >
+  );
+};
+
+export default MainView;
