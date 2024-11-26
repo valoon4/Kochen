f661ed4258ab107c936a03947c869e032004e914
valoon4 <jenjasy@web.de>
2024-11-26 11:13:43 +0100

    Suppen erg-änzt

--- src/pages/Speisekarte.tsx
+++ src/pages/Speisekarte.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationManager } from '@/utils/NavigationManager';
 
 const MainView = () => {
+  
   return (
     <main>
       <h1>Cooking Space</h1>
@@ -9,19 +10,294 @@ const MainView = () => {
 
       {/* Abschnitt für Suppe */}
       <div className="section">
-        <h2 className="abschnitt">Suppe</h2>
+        <details className="abschnitt"><summary className="abschnittname">Suppen</summary>
+        <p> Dazu nach Wahl Brot, Croutons, Backerbsen.</p>
         <div className="menu">
+        <div className="menu-item">
+            <h3>Avocadosuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
           <div className="menu-item">
-            <h3>Tomatensuppe</h3>
-            <p>Frische Tomaten, Basilikum und ein Hauch von Knoblauch</p>
+            <h3>Biersuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Apfelsuppe</h3>
+            <p> Äpfel, Pastinaken</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Blumenkohlsuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Bohnensuppe zb Pasulj</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Borscht (Rote Bete Suppe)</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Brennnesselsuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Brokkolisuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Cesnecka (Knoblauchsuppe)</h3>
+            <p> TBA</p>
+            <p className="price">€3,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Cheeseburgersuppe</h3>
+            <p> TBA</p>
+            <p className="price">€3,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Chillicheesesuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Currysuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Currywurstsuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Dalithoy (gelbe Linsensuppe)</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Erbsensuppe</h3>
+            <p> Erbsen, Minze</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Erdnusssuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Ezogelinsuppe</h3>
+            <p> Bulgur, rote Linsen</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Fenchelsuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Gazpacho (kalte Tomatensuppe) / Salmorejo</h3>
+            <p> TBA</p>
+            <p className="price">€2</p>
+          </div>
+          <div className="menu-item">
+            <h3>Gochujang Gazpacho</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>          <div className="menu-item">
+            <h3>Gochujangsuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Gurkensuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Ingwersuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Karottensuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Kartoffelsuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Kesäkeitto</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Kimchisuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Kürbissuppe</h3>
+            <p>Kürbis, Knoblauch, Thymian, Kokosmilch, Ingwer, Zimt</p>
+            <p className="price">€6,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Lasagnesuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Lauchsuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Limettensuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Linsensuppe</h3>
+            <p> Linsen, Karotten, Stangensellerie, Zwiebel, Gemüsebrühe</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Maissuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Minestrone</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Mulligatawny Suppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Nudelsuppe (Ramen)</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>          <div className="menu-item">
+            <h3>Maronensuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Paprikasuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Pfeffersuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Pilzsuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Pizzasuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Purée Mongole (Gelbe Erbsensuppe)</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Rosenkohlsuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Rotkohlsuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Rucolasuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Rumfordsuppe</h3>
+            <p> Graupen</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Selleriesuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Shchi</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Spargelsuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Spinatsuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Tacosuppe</h3>
+            <p>Geröstete Tomaten, Knoblauch</p>
             <p className="price">€5,00</p>
           </div>
           <div className="menu-item">
-            <h3>Kürbiscremesuppe</h3>
-            <p>Würzige Kürbissuppe mit Ingwer und Kokosmilch</p>
-            <p className="price">€6,00</p>
+            <h3>Tomatensuppe</h3>
+            <p>Geröstete Tomaten, Knoblauch</p>
+            <p className="price">€5,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Tom Yum Suppe</h3>
+            <p>Zitronengras, Galgant, Kaffernlimettenblätter</p>
+            <p className="price">€5,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Vichyssoise</h3>
+            <p> Kartoffel, Lauch</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Walnusssuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Zucchinisuppe</h3>
+            <p> Zucchini, Gurke, Avocado, Minze, Gemüsebrühe</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Zwiebelsuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
           </div>
         </div>
+        </details>  
       </div>
 
       {/* Abschnitt für Hauptspeisen */}
@@ -48,6 +324,11 @@ const MainView = () => {
             <p>Auberginen, Zucchini, Tomaten und Ricotta</p>
             <p className="price">€10,50</p>
           </div>
+          <div className="menu-item">
+            <h3>TBA</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
         </div>
       </div>
 
@@ -55,6 +336,26 @@ const MainView = () => {
       <div className="section">
         <h2 className="abschnitt">Dessert</h2>
         <div className="menu">
+        <div className="menu-item">
+            <h3>Bananensuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+        <div className="menu-item">
+            <h3>Bilo-bilo & Binignit</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Hagebuttensuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Mandelsuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
           <div className="menu-item">
             <h3>Schokoladenkuchen</h3>
             <p>Frisch gebacken, mit einer Portion Vanilleeis</p>
@@ -64,6 +365,21 @@ const MainView = () => {
             <h3>Apfelstrudel</h3>
             <p>Serviert mit Schlagsahne und Zimt</p>
             <p className="price">€5,50</p>
+          </div>
+          <div className="menu-item">
+            <h3>Sauerkirschsuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Schwarze Sesamsuppe</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
+          </div>
+          <div className="menu-item">
+            <h3>Talbina</h3>
+            <p> TBA</p>
+            <p className="price">€2,00</p>
           </div>
         </div>
       </div>
