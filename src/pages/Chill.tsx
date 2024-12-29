f3c68a4bcd6f6d148907a589c04607d090f01050
valoon4 <jenjasy@web.de>
2024-12-29 01:43:23 +0100

    Update Chill.tsx

--- src/pages/Chill.tsx
+++ src/pages/Chill.tsx
@@ -3,24 +3,24 @@ import { NavigationManager } from '@/utils/NavigationManager';
 
 const MainView = () => {
   const tilesData = [
-    { title: 'Anime' },
-    { title: 'Brettspiele' },
-    { title: 'Cartoons' },
-    { title: 'Comics' },
-    { title: 'Finanzen' },
-    { title: 'Fitness' },
-    { title: 'Games' },
-    { title: 'Haushalt' },
-    { title: 'Japanisch' },
-    { title: 'Kochen' },
-    { title: 'Live Action' },
-    { title: 'Manga' },
-    { title: 'Musik' },
-    { title: 'Nostalgie Pics & Vids' },
-    { title: 'Story' },
-    { title: 'Videogame' },
-    { title: 'Work' },
-    { title: 'Zeichnen' },
+    { title: 'Anime', color: '#90EE90' }, // hellgrün
+    { title: 'Brettspiele', color: '#A52A2A' }, // braun
+    { title: 'Cartoons', color: '#FFA500' }, // orange
+    { title: 'Comics', color: '#FFFFFF' }, // default weiß
+    { title: 'Finanzen', color: '#FFD700' }, // dunkelgelb
+    { title: 'Fitness', color: '#ADD8E6' }, // hellblau
+    { title: 'Games', color: '#000000' }, // schwarz
+    { title: 'Haushalt', color: '#808080' }, // grau
+    { title: 'Japanisch', color: '#FFFFFF' }, // weiß
+    { title: 'Kochen', color: '#006400' }, // dunkelgrün
+    { title: 'Live Action', color: '#D2B48C' }, // hellbraun
+    { title: 'Manga', color: '#228B22' }, // mittelgrün
+    { title: 'Musik', color: '#00008B' }, // dunkelblau
+    { title: 'Nostalgie Pics & Vids', color: '#C0C0C0' }, // silber
+    { title: 'Story', color: '#800080' }, // lila
+    { title: 'Videogame', color: '#C0C0C0' }, // silber
+    { title: 'Work', color: '#8B0000' }, // dunkelrot
+    { title: 'Zeichnen', color: '#FFC0CB' }, // pink
   ];
 
   return (
@@ -29,7 +29,10 @@ const MainView = () => {
       <NavigationManager />
       <div style={styles.tilesContainer}>
         {tilesData.map((tile, index) => (
-          <div key={index} style={styles.tile}>
+          <div
+            key={index}
+            style={{ ...styles.tile, backgroundColor: tile.color }}
+          >
             <h2 style={styles.title}>{tile.title}</h2>
           </div>
         ))}
@@ -50,9 +53,9 @@ const styles: { [key: string]: React.CSSProperties } = {
     border: '1px solid #ddd',
     padding: '16px',
     borderRadius: '8px',
-    backgroundColor: '#f9f9f9',
     display: 'flex',
     flexDirection: 'column', // Details untereinander anzeigen
+    color: '#fff', // Schriftfarbe für Kontrast
   },
   title: {
     marginTop: 0,
