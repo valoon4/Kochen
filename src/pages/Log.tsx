c67feb3430325cb320d553cdad960ca775987f78
valoon4 <jenjasy@web.de>
2025-01-08 21:56:38 +0100

    Update Log.tsx

--- src/pages/Log.tsx
+++ src/pages/Log.tsx
@@ -17,6 +17,11 @@ const MainView = () => {
       <summary>Manga Updates in 2025 </summary>
       <p>Shigatsu wa Kimi no Uso</p>
       <p>Sanda</p>
+      <p>DRCL - Midnight Children</p>
+      <p>Watashi no Musuko ga Isekai Tensei Shitappoi</p>
+      <p>Onee Shikkaku - Kedamono ni Hyouhen shita Gozen Sanji</p>
+      <p>Kyousei Kekkon - Gaten Nikushoku Danshi x Intelli Soushoku Joshi</p>
+      <p>Sanda</p>
       </details><br />
 
       <details>
@@ -27,10 +32,22 @@ const MainView = () => {
       <details>
       <summary>Comic Updates in 2025</summary>
       <p>Anne Bonny - Die Wölfin der Karibik</p>
+      <p>Beate & Serge Klarsfeld - Die Nazijäger</p>
+      <p>Charlie Chaplin - Die Comic-Biografie</p>
       <p>Colt & Coal</p>
+      <p>Die zwei Leben von Balduin</p>
+      <p>Ducks - Zwei Jahre in den Ölsanden</p>
+      <p>Gebrandmarkt</p>
       <p>Historische Persönlichkeiten</p>
+      <p>Hit Girl</p>
+      <p>Hoka Hey!</p>
+      <p>Kick Ass - Frauenpower</p>
       <p>Kriege und Drachen</p>
       <p>Western Love</p>
+      <p>Die Ritter von Heliopolis Band 04</p>
+      <p>Kick Ass Band 05 (Kick Ass 3)</p>
+      <p>Mythen der Antike Band 13 & 14</p>
+      <p>Star Wars Special Band 101 - 120</p>
       </details><br />
 
       <details>
